2bf2312927220ac834b8992820f6448fd167ca48
klm127 <karlmiller127@gmail.com>
2023-04-15 16:24:23 -0400

    manifest v2, debug mode

--- src/communication/fetcher.ts
+++ src/communication/fetcher.ts
@@ -26,6 +26,14 @@ export class Fetcher {
         myheaders.set("Content-Type", "application/json")
         myheaders.set(cookie_name, this.token)
 
+        console.log("fetching from", targetURL)
+
+        let h_list :string[] = []
+        myheaders.forEach( (v,k,p)=> {
+            h_list.push(k)
+        })
+        console.log("have headers", h_list)
+
         await fetch(targetURL, {
             method: httpmethod,
             mode: "cors",
@@ -37,6 +45,8 @@ export class Fetcher {
         }).then((jsonArray)=> {
             this.responses = jsonArray
         }).catch((error)=> {
+            console.log("ERROR FETCHING",error)
+            console.log("ERROR TYPE", typeof error)
             let message : Server.Message = {
                 Success: false,
                 Text: "Error fetching from server!"
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import { Cafe } from './Cafe'
+import { Debug } from './debug/debug'
 import './style.css'
 
 const cafe = new Cafe()
 document.body.append(cafe.navbar.el)
+
+const debug_mode : string = import.meta.env.VITE_EXTENSION_DEBUG_MODE
+if(debug_mode == "true") {
+    console.log("DEBUG MODE")
+    const debug = new Debug()
+    document.body.append(debug.el)
+    debug.log({a:1,b:2})
+    debug.log([1,2,3])
+    window.console.log = debug.log.bind(debug)
+
+    
+
+}
+
+
+
 console.log("Comment Anywhere Front End running:", cafe);
