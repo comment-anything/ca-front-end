e446c9e635abe3756d03103fff7fedd1b18fa638
klm127 <karlmiller127@gmail.com>
2023-04-16 21:24:31 -0400

    browser undefined bugfix patch

--- src/contentscript/content.ts
+++ src/contentscript/content.ts
@@ -3,28 +3,32 @@ import { Debug } from "./debug/debug"
 import { T_ExtensionMsg as M } from "../communication/EXTENSTION"
 
 
-const debug_mode : string = import.meta.env.VITE_EXTENSION_DEBUG_MODE
-if(debug_mode == "true") {
-    const debug = new Debug()
-    document.body.append(debug.getStyle())
-    document.body.append(debug.el)
-    window.console.log = debug.log.bind(debug)
-    if(typeof browser != undefined) {
-        console.log("browser.extension.getURL:" , browser.extension.getURL(""))
-        console.log("browser.runtime.getURL:", browser.runtime.getURL(""))
-        console.log("window.location.href", window.location.href)
-    }    
-}
+if(typeof browser != "undefined") {
 
-let port : M.Content.Port = browser.runtime.connect({name: "cscript-port"}) as any
-
-port.onMessage.addListener( (r)=> {
-    if(r.command == "message") {
-        console.log(r.data)
-    } else if(r.command == "send-href") {
-        port.postMessage({
-            type: "href",
-            data: window.location.href
-        })
+    const debug_mode : string = import.meta.env.VITE_EXTENSION_DEBUG_MODE
+    if(debug_mode == "true") {
+        const debug = new Debug()
+        document.body.append(debug.getStyle())
+        document.body.append(debug.el)
+        window.console.log = debug.log.bind(debug)
+        if(typeof browser != undefined) {
+            console.log("browser.extension.getURL:" , browser.extension.getURL(""))
+            console.log("browser.runtime.getURL:", browser.runtime.getURL(""))
+            console.log("window.location.href", window.location.href)
+        }    
     }
-})
+
+    let port : M.Content.Port = browser.runtime.connect({name: "cscript-port"}) as any
+
+    port.onMessage.addListener( (r)=> {
+        if(r.command == "message") {
+            console.log(r.data)
+        } else if(r.command == "send-href") {
+            port.postMessage({
+                type: "href",
+                data: window.location.href
+            })
+        }
+    })
+
+}
